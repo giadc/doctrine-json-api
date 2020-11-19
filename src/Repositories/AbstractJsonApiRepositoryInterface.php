5772a8ea8a4b6562af8a3f03a3288c541eb80f80
dhill <dhill2@gannett.com>
2020-11-19 10:02:27 -0500

    Ensure nested includes have valid relationships.

--- src/Repositories/AbstractJsonApiRepositoryInterface.php
+++ src/Repositories/AbstractJsonApiRepositoryInterface.php
@@ -1,103 +1,133 @@
 <?php
 namespace Giadc\DoctrineJsonApi\Repositories;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Tools\Pagination\Paginator;
-use Giadc\JsonApiRequest\Requests\Filters;
 use Giadc\JsonApiRequest\Requests\Includes;
-use Giadc\JsonApiRequest\Requests\Pagination;
 use Giadc\JsonApiRequest\Requests\RequestParams;
-use Giadc\JsonApiRequest\Requests\Sorting;
 
+/**
+ * @template Entity of \Giadc\JsonApiResponse\Interfaces\JsonApiResource
+ */
 interface AbstractJsonApiRepositoryInterface
 {
     /**
-     * Paginate entities with Includes, Sorting, and Filters
+     * Paginate entities with Includes, Sorting, and Filters.
      *
-     * @param  RequestParams $params
-     * @param  array $additionalIncludes
-     * @return Doctrine\ORM\Tools\Pagination\Paginator
+     * @phpstan-param array<string> $additionalIncludes
+     *
+     * @return Paginator
+     * @phpstan-return Paginator<Entity>
      */
-    public function paginateAll(RequestParams $params, array $additionalIncludes = []);
+    public function paginateAll(
+        RequestParams $params,
+        array $additionalIncludes = []
+    );
 
     /**
-     * Find an entity by ID
+     * Find an entity by ID.
      *
-     * @param  string        $value
-     * @param  Includes|null $includes
-     * @return mixed
+     * @param string | int $value
+     *
+     * @return ?object
+     * @phpstan-return Entity | null
      */
-    public function findById(string $value, Includes $includes = null);
+    public function findById($value, Includes $includes = null);
 
     /**
-     * Find entity by field value
+     * Find entity by field value.
      *
-     * @param  mixed         $value
-     * @param  string        $field
-     * @param  Includes|null $includes
-     * @return mixed
+     * @param  mixed $value
+     *
+     * @return ?object
+     * @phpstan-return Entity | null
      */
-    public function findOneByField($value, string $field = 'id', Includes $includes = null);
+    public function findOneByField(
+        $value,
+        string $field = 'id',
+        Includes $includes = null
+    );
 
     /**
-     * Find entities by field value
+     * Find entities by field value.
      *
-     * @param  mixed         $value
-     * @param  string        $field
-     * @param  Includes|null $includes
+     * @param mixed $value
+     *
      * @return ArrayCollection
+     * @phpstan-return ArrayCollection<string | int, Entity>
      */
-    public function findByField($value, string $field = 'id', Includes $includes = null);
+    public function findByField(
+        $value,
+        string $field = 'id',
+        Includes $includes = null
+    );
 
     /**
-     * Find enties by an array of field values
+     * Find entities by an array of field values.
      *
-     * @param  array         $array
-     * @param  string        $field
-     * @param  Includes|null $includes
+     * @phpstan-param array<mixed> $array
+     *
      * @return ArrayCollection
+     * @phpstan-return ArrayCollection<string | int, Entity>
      */
-    public function findByArray(array $array, string $field = 'id', Includes $includes = null);
+    public function findByArray(
+        array $array,
+        string $field = 'id',
+        Includes $includes = null
+    );
 
     /**
-     * Updates or creates an Entity
+     * Updates or creates an Entity.
      *
-     * @param $entity
-     * @return mixed
+     * @param object $entity
+     * @phpstan-param Entity $entity
+     *
+     * @return void
      */
     public function createOrUpdate($entity);
 
     /**
-     * Update an existing Entity
+     * Update an existing Entity.
      *
-     * @param $entity
+     * @param object $entity
+     * @phpstan-param Entity $entity
+     *
      * @return void
      */
     public function update($entity, bool $mute = false);
 
     /**
-     * Add a new Entity to the database
+     * Add a new Entity to the database.
      *
-     * @param mixed   $entity
-     * @param boolean $mute
+     * @param object $entity
+     * @phpstan-param Entity $entity
+     *
+     * @return void
      */
     public function add($entity, bool $mute = false);
 
     /**
-     * Delete an Entity from the database
+     * Delete an Entity from the database.
      *
-     * @param  mixed   $entity
-     * @param  boolean $mute
+     * @param object $entity
+     * @phpstan-param Entity $entity
+     *
+     * @return void
      */
     public function delete($entity, bool $mute = false);
 
     /**
-     * Flush pending changes to the database
+     * Flush pending changes to the database.
+     *
+     * @return void
      */
     public function flush();
 
     /**
      * Clears the EntityManager. All entities that are currently managed
      * by this EntityManager become detached.
+     *
+     * @return void
      */
     public function clear();
 }
--- src/Repositories/Processors.php
+++ src/Repositories/Processors.php
@@ -1,171 +1,17 @@
 <?php
 namespace Giadc\DoctrineJsonApi\Repositories;
 
+use Doctrine\ORM\Mapping\ClassMetadata;
 use Doctrine\ORM\QueryBuilder;
 use Doctrine\ORM\Tools\Pagination\Paginator;
+use Doctrine\Persistence\Mapping\MappingException;
 use Giadc\JsonApiRequest\Requests\Includes;
 use Giadc\JsonApiRequest\Requests\Pagination;
 use Giadc\JsonApiRequest\Requests\Sorting;
 
+/**
+ * @template Entity of \Giadc\JsonApiResponse\Interfaces\JsonApiResource
+ */
 trait Processors
 {
-    /**
-     * Add SELECT and LEFT JOIN statements for all includes
-     *
-     * @param  QueryBuilder $qb
-     * @param  Includes     $includes
-     * @return QueryBuilder
-     */
-    protected function processIncludes(QueryBuilder $qb, Includes $includes): QueryBuilder
-    {
-        foreach ($includes->toArray() as $include) {
-            if (!$this->hasAssociation($include) || $this->includeExists($qb, $include)) {
-                continue;
-            }
-
-            (strpos($include, '.') === false)
-                ? $this->processSingleInclude($qb, $include)
-                : $this->processNestedInclude($qb, $include);
-        }
-
-        return $qb;
-    }
-
-    /**
-     * Process a single include
-     *
-     * @param  QueryBuilder $qb
-     * @param  string       $include
-     * @return void
-     */
-    protected function processSingleInclude(QueryBuilder $qb, string $include)
-    {
-        $qb->leftJoin('e.' . $include, $include);
-        $qb->addSelect($include);
-    }
-
-    /**
-     * Process nested includes
-     *
-     * @param  QueryBuilder $qb
-     * @param  string       $include
-     * @return void
-     */
-    protected function processNestedInclude(QueryBuilder $qb, string $include)
-    {
-        $includePieces = explode('.', $include);
-        array_unshift($includePieces, 'e');
-
-        for ($i = 1; $i < count($includePieces); $i++) {
-            if ($this->includeExists($qb, $includePieces[$i])) {
-                continue;
-            }
-
-            $qb->leftJoin($includePieces[$i - 1] . '.' . $includePieces[$i], $includePieces[$i]);
-            $qb->addSelect($includePieces[$i]);
-        }
-    }
-
-    /**
-     * Determines if an include has already been processed
-     *
-     * @param  QueryBuilder $qb
-     * @param  string       $include
-     * @return bool
-     */
-    protected function includeExists(QueryBuilder $qb, string $include): bool
-    {
-        foreach ($qb->getDQLPart('join') as $joins) {
-            foreach ($joins as $join) {
-                if ($join->getAlias() === $include) {
-                    return true;
-                }
-            }
-        }
-
-        return false;
-    }
-
-    /**
-     * Process Sorting
-     *
-     * @param  QueryBuilder $qb
-     * @param  Sorting      $sort
-     * @return QueryBuilder
-     */
-    protected function processSorting(QueryBuilder $qb, Sorting $sort): QueryBuilder
-    {
-        $sortArray = $sort->toArray();
-
-        if (empty(array_filter($sortArray))) {
-            $sortArray = $this->getDefaultSort();
-        }
-
-        foreach ($sortArray as $key => $field) {
-            if ($this->hasField($field['field'])) {
-                $qb->addOrderBy('e.' . $field['field'], $field['direction']);
-            } elseif ($this->hasAssociation($field['field'])) {
-                $qb->addOrderBy($field['field'], $field['direction']);
-            }
-        }
-
-        return $qb;
-    }
-
-    /**
-     * Add offset and page size
-     *
-     * @param  QueryBuilder $query
-     * @param  Pagination   $page
-     * @return Paginator
-     */
-    public function paginate(QueryBuilder $query, Pagination $page): Paginator
-    {
-        $query = $query->setParameters($query->getParameters())
-            ->setFirstResult($page->getOffset() * $page->getPageSize())
-            ->setMaxResults($page->getPageSize());
-
-        return new Paginator($query, true);
-    }
-
-    /**
-     * Determine if the current Class has a field
-     *
-     * @param  string  $field
-     * @return boolean
-     */
-    protected function hasField(string $field): bool
-    {
-        $fields = $this->getClassMetadata($this->class);
-
-        return $fields->hasField($field);
-    }
-
-    /**
-     * Determine if the current Class has an association
-     *
-     * @param  string  $association
-     * @return boolean
-     */
-    protected function hasAssociation(string $association): bool
-    {
-        $associations = $this->getClassMetadata()->getAssociationMappings();
-
-        if (strpos($association, '.')) {
-            $associationPieces = explode('.', $association);
-            return array_key_exists($associationPieces[0], $associations);
-        }
-
-        return array_key_exists($association, $associations);
-    }
-
-    /**
-     * Return the metadata mapping for the current Class
-     *
-     * @return array
-     */
-    protected function getClassMetadata()
-    {
-        return $this->em->getClassMetadata($this->class);
-    }
 }
