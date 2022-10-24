97176131a140ea1ac000a96baf274bd602b3ac93
Ryan Marganti <rmarganti@gannett.com>
2022-10-24 04:04:38 -0400

    GCCDEV-4180 Fix return typing on FilterManager@getValidKey(). (#7)
    * GCCDEV-4180 Fix return typing on FilterManager@getValidKey().
    
    * When single value, use  instead of  in buildMultiple().

--- src/Filters/FilterManager.php
+++ src/Filters/FilterManager.php
@@ -77,6 +77,8 @@ abstract class FilterManager
     }
 
     /**
+     * @phpstan-param string[] $data
+     *
      * @throws \Exception
      */
     private function processFilter(string $key, array $data): void
@@ -95,13 +97,15 @@ abstract class FilterManager
 
     /**
      * @phpstan-param FilterTypes $info
+     * @phpstan-return string | string[]
      */
-    private function getValidKey(array $info, string $key): string
+    private function getValidKey(array $info, string $key): string | array
     {
         return $key = isset($info['key']) ? $info['key'] : $key;
     }
 
     /**
+     * @phpstan-param string[] $data
      * @phpstan-param string|string[] $key
      */
     private function idBuilder(array $data, string|array $key): void
@@ -113,17 +117,22 @@ abstract class FilterManager
         }
     }
 
-    private function getKey(string $key): string
+    /**
+     * Build the DQL reference for the given column name. If the column is in a
+     * relationship, ensure that the relationship is is included in the DQL joins.
+     */
+    private function getKey(string $column): string
     {
-        if (strpos($key, ".") !== false) {
-            $this->addInclude(explode('.', $key)[0]);
-            return $key;
+        if (strpos($column, ".") !== false) {
+            $this->addInclude(explode('.', $column)[0]);
+            return $column;
         }
 
-        return 'e.' . $key;
+        return 'e.' . $column;
     }
 
     /**
+     * @phpstan-param string[] $data
      * @phpstan-param string[] $keys
      */
     private function buildMultiple(array $data, array $keys): void
@@ -131,17 +140,34 @@ abstract class FilterManager
         $conditions = [];
 
         foreach ($keys as $field) {
-            $conditions[] = $this->qb->expr()
-                ->in($this->getKey($field), '?' . $this->paramInt);
+            if (count($data) === 1) {
+                $conditions[] = $this->qb->expr()->eq(
+                    $this->getKey($field),
+                    '?' . $this->paramInt
+                );
+            } else {
+                $conditions[] = $this->qb->expr()->in(
+                    $this->getKey($field),
+                    '?' . $this->paramInt
+                );
+            }
         }
 
         $orX = $this->qb->expr()->orX();
         $orX->addMultiple($conditions);
 
         $this->qb->andWhere($orX);
-        $this->setParameter($this->paramInt, $data);
+
+        if (count($data) === 1) {
+            $this->setParameter($this->paramInt, $data[0]);
+        } else {
+            $this->setParameter($this->paramInt, $data);
+        }
     }
 
+    /**
+     * @phpstan-param string[] $data
+     */
     private function buildSingle(array $data, string $key): void
     {
         if (is_array($data) && count($data) > 1) {
@@ -158,6 +184,7 @@ abstract class FilterManager
     }
 
     /**
+     * @phpstan-param string[] $data
      * @phpstan-param string|string[] $keys
      */
     private function keywordBuilder(array $data, string|array $keys): void
@@ -175,6 +202,7 @@ abstract class FilterManager
     }
 
     /**
+     * @phpstan-param string[] $data
      * @phpstan-param string[] $keys
      * @phpstan-return array<\Doctrine\ORM\Query\Expr\Comparison>
      */
@@ -190,6 +218,7 @@ abstract class FilterManager
     }
 
     /**
+     * @phpstan-param string[] $data
      * @phpstan-return array<\Doctrine\ORM\Query\Expr\Comparison>
      */
     private function buildSingleConditions(array $data, string $key): array
@@ -209,6 +238,9 @@ abstract class FilterManager
         return $conditions;
     }
 
+    /**
+     * @phpstan-param string[] $data
+     */
     protected function nullBuilder(array $data, string $key): void
     {
         if (is_array($data)) {
@@ -235,6 +267,8 @@ abstract class FilterManager
 
     /**
      * Build a `combined` filter
+     *
+     * @phpstan-param string[] $data
      */
     private function combinedBuilder(array $data, string $key): void
     {
@@ -252,6 +286,7 @@ abstract class FilterManager
     /**
      * Build a single `combined` condition
      *
+     * @phpstan-param string[] $data
      * @phpstan-param string[] $keys
      * @phpstan-return array<\Doctrine\ORM\Query\Expr\Comparison>
      */
@@ -302,6 +337,7 @@ abstract class FilterManager
     }
 
     /**
+     * @phpstan-param string[] $data
      * @phpstan-param string|string[] $keys
      * @throws \Exception
      */
@@ -324,6 +360,9 @@ abstract class FilterManager
         return $this->qb->andWhere($sql);
     }
 
+    /**
+     * @phpstan-param string[] $data
+     */
     private function buildSingleDate(array $data, string $key): string
     {
         if (is_array($data)) {
